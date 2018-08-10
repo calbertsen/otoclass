d9d9e76bdd8a26bb01b42c14829324006ffbceb9
Christoffer Moesgaard Albertsen <ca@calbertsen.dk>
2018-08-10 14:10:33 +0200

    Add arg to fcbf to control number of bins used - default changed!

--- otoclass/R/fcbf.R
+++ otoclass/R/fcbf.R
@@ -30,31 +30,51 @@ SU <- function(x,y,nx=5,ny=5){
 ##' @param group A vector of groups
 ##' @param delta Tuning parameter
 ##' @param lambda Tuning parameter
+##' @param nCuts Function that calculates number of bins used for calculating correlations
 ##' @return A vector of selected features
 ##' @author Christoffer Moesgaard Albertsen
 ##' @importFrom stats na.omit
 ##' @export
-fcbf <- function(train,group,delta,lambda=1){
+fcbf <- function(train,
+                  group,
+                  delta,
+                  lambda=1,
+                  nCuts = function(x){round(length(unique(x)) / 5)}){
     # http://www.cs.waikato.ac.nz/ml/publications/1997/Hall-LSmith97.pdf
-    # http://pdf.aminer.org/000/335/746/feature_selection_for_high_dimensional_data_a_fast_correlation_based.pdf
+                                        # http://pdf.aminer.org/000/335/746/feature_selection_for_high_dimensional_data_a_fast_correlation_based.pdf
+    ## Exclude variables with no variance
     Slist <- numeric(0)
-    vals <- apply(train,2,function(x)SU(x,group))
+    vals <- apply(train,2,function(x){
+        SU(x,group,nx = max(2,nCuts(x)))
+    })
     Slist <- order(vals,decreasing=TRUE)
-    Slist <- Slist[vals[Slist]>=delta]
+    ## Exclude variables with no variance
+    hasVar <- apply(train,2,function(x)!isTRUE(all.equal(sd(x),0)))
+    Slist <- Slist[vals[Slist]>=delta & hasVar[Slist]]
+    if(length(Slist) == 0)
+        stop("There are no variables that are more than delta correlated to the groups.")
+    if(length(Slist) == 1)
+        return(Slist)
     N <- length(Slist)
     for(p in 1:N){
         if(!is.na(Slist[p])){
             indxUse <- na.omit(Slist[-(1:p)])
             trainUse <- train[,indxUse]
             if(class(trainUse)=="numeric"){
-                valp <- SU(trainUse,train[,Slist[p]])
+                nT1 <- max(2,nCuts(trainUse))
+                nT2 <- max(2,nCuts(train[,Slist[p]]))
+                valp <- SU(trainUse,train[,Slist[p]],nT1,nT2)
                 Slist[indxUse[valp >= lambda*vals[stats::na.omit(Slist[-(1:p)])]]] <- NA
             }else if(dim(trainUse)[2]>0){
-                valp <- apply(train[,stats::na.omit(Slist[-(1:p)])],2,function(x)SU(x,train[,Slist[p]]))
+                valp <- apply(train[,stats::na.omit(Slist[-(1:p)])],2,function(x){
+                    nT1 <- max(2,nCuts(x))
+                    nT2 <- max(2,nCuts(train[,Slist[p]]))
+                    SU(x,train[,Slist[p]],nT1,nT2)
+                })
                 Slist[Slist%in%indxUse[valp >= lambda*vals[stats::na.omit(Slist[-(1:p)])]]] <- NA
             }
         }        
     }
-    return(as.vector(na.omit(Slist)))
+     return(as.vector(na.omit(Slist)))
 }
 
